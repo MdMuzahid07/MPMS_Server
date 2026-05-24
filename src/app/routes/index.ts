dfa80401db6ce5eadc1c9f5c162a42af2cec708b
MdMuzahid07 <mdmuzahid7396@gmail.com>
2026-05-24 07:10:29 +0600

    route imports , index

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,15 @@
 import express from "express";
+
+import { ActivityRoutes } from "../modules/activity/activity.routes";
 import { AuthRoutes } from "../modules/auth/auth.routes";
+import { CommentRoutes } from "../modules/comment/comment.routes";
+import { ProjectRoutes } from "../modules/project/project.routes";
+import { ReportRoutes } from "../modules/report/report.routes";
+import { SprintRoutes } from "../modules/sprint/sprint.routes";
+import { globalTaskRouter, sprintTaskRouter } from "../modules/task/task.routes";
+import { TimeLogRoutes } from "../modules/timelog/timelog.routes";
+import { UploadRoutes } from "../modules/upload/upload.routes";
+import { UserRoutes } from "../modules/user/user.routes";
 
 const router = express.Router();
 
@@ -7,6 +17,46 @@ const moduleRoutes = [
   {
     path: "/auth",
     route: AuthRoutes,
+  },
+  {
+    path: "/activity",
+    route: ActivityRoutes,
+  },
+  {
+    path: "/comment",
+    route: CommentRoutes,
+  },
+  {
+    path: "/project",
+    route: ProjectRoutes,
+  },
+  {
+    path: "/report",
+    route: ReportRoutes,
+  },
+  {
+    path: "/sprint",
+    route: SprintRoutes,
+  },
+  {
+    path: "/tasks",
+    route: globalTaskRouter,
+  },
+  {
+    path: "/sprint-task",
+    route: sprintTaskRouter,
+  },
+  {
+    path: "/timelog",
+    route: TimeLogRoutes,
+  },
+  {
+    path: "/upload",
+    route: UploadRoutes,
+  },
+  {
+    path: "/user",
+    route: UserRoutes,
   },
 ];
 
